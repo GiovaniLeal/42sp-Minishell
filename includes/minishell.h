2ece9c948c52cc4123a21536808c19655d09c2f8
GiovaniLeal <giovaniandradeleal@gmail.com>
2026-02-13 19:28:33 -0300

    add: adicão de funcoes auxiliares do lexer

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,12 +11,21 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "ft_printf.h"
 #include <stdlib.h>
 #include <readline/readline.h>
 
 /* ************************************************************************** */
 /*  		     TOKEN PART - ENUM AND LIST	        	        */
 /* ************************************************************************** */
+/* 							        */
+/* Esse enum simplifica a implementacao do lexer quanto a aspas simples e duplas*/
+typedef enum e_state
+{
+	STATE_GENERAL,
+	STATE_IN_SQUOTE,
+	STATE_IN_DQOTE
+}	t_state;
 /* 							        */
 /*   Esse enum representa os operadores(tokens) esperados do nosso shell.     */
 /*EX.:		T_WORD :     "echo " 		                  */
@@ -40,23 +49,31 @@ typedef struct s_token
 { 
 	char		*value;
 	t_token_type	type;
-	s_token		*next;	
+	struct s_token		*next;	
 }	t_token;
 
 
 /* ************************************************************************** */
 
 
-typedef enum e_state
-{
-	STATE_GENERAL,
-	STATE_IN_SQUOTE,
-	STATE_IN_DQOTE
-}	t_state;
+
 
 
 
 int	main(void);
-/* lexer.c --------------------------------*/
+
+/* lexer.c ---------------------------------------------*/
 t_token	*lexer(char *str);
-int	get_token_type_and_len(t_token_type *node_type, char *str);
+void	free_token_list(t_token *token_list);
+
+/* lexer_utils.c ---------------------------------------------*/
+int	is_operator(char c);
+int	is_white_space(char c);
+void	free_token_list(t_token *token_list);
+t_token	*ft_lst_last_node(t_token *lst_tokens);
+void	add_back_token_lst(t_token **lst_tokens, t_token *new_token);
+
+
+/* EXCLUIR ANTES DO ENVIO - lexer_tester.c -------------*/
+void	print_tokens(t_token  *tokens_list);
+char	*token_type_to_str(t_token_type type);
