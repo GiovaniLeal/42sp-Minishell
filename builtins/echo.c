6a03ac82d2f8108e85d7471acad62d1c8af7b65c
Giovani Dos Santos De Andrade Leal <giodos-s@c1r5p1.42sp.org.br>
2026-03-31 17:56:14 -0300

    feat: add echo built-in

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,46 +6,48 @@
 /*   By: giodos-s <giodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 15:13:54 by giodos-s          #+#    #+#             */
-/*   Updated: 2026/03/31 15:48:50 by giodos-s         ###   ########.fr       */
+/*   Updated: 2026/03/31 17:17:35 by giodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int exec_echo_n(t_ast *node)
+static int check_option(char **str_array)
 {
-    (void)node;
-    return (1);
-}
+    int count;
 
-int exec_simple_echo(t_ast *node)
-{
-    while (node->next != null)
-    {int exec_echo(t_ast *node)
-        ft_printf("%s ", node);
-        node = node->next;
-    }
-    ft_printf("%s\n", node);
-    return(0);
-}
-
-
-//Identifica se possui a opcao [ -n ] - retorna 0 se nao encontrar opcao e 1 se encontrar.
-int echo_option(t_ast *node)
-{
-    while (node[0] != '-' && node[1] != 'n')
+    count = 0;
+    while (str_array[count] && ft_strcmp(str_array[count], "-n") == 0)
     {
-        if (node->next == NULL)
-            return (0) 
-        node = node->next;
+        count++;
     }
-    return (1);
+    return (count);
 }
 
-
-int exec_echo(t_ast *node)
+int exec_echo(char **str_array)
 {
-    if (echo_option(node))
-        return (exec_echo_n(node));
-    return (exec_simple_echo(node));
+    int i;
+    int has_n_option;
+
+    i = 1;
+    has_n_option = check_option(str_array);
+
+    if (has_n_option)
+    {
+        while (str_array[has_n_option + i])
+        {
+            ft_printf("%s", str_array);
+            has_n_option++;
+        }
+    }
+    else
+    {
+        while (str_array[i])
+        {
+            ft_printf("%s ", str_array[i]);
+            i++;
+        }
+        ft_printf("\n");
+    }   
+    return (0);
 }
--- includes/buildtins.h
+++ includes/buildtins.h
@@ -6,7 +6,7 @@
 /*   By: giodos-s <giodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 14:25:56 by giodos-s          #+#    #+#             */
-/*   Updated: 2026/03/31 15:51:02 by giodos-s         ###   ########.fr       */
+/*   Updated: 2026/03/31 17:07:36 by giodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ int	exec_builtins(t_ast *node, t_shell *shell);
 int	ft_exit(char **argv);
 int	is_numeric(char	*str);
 
-int exec_echo(char **node);
-int exec_echo_n(char **node);
-int exec_simple_echo(char **node);
-int echo_option(char **node);
+int exec_echo(char **str_array);
+
 
 
