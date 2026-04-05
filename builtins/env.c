5e770662b47d4d33457210faaef28dab14882fb1
GiovaniLeal <giovaniandradeleal@gmail.com>
2026-04-05 16:51:05 -0300

    feat: atualizando branch

---
+++ builtins/env.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: giodos-s <giodos-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/05 16:15:45 by giodos-s          #+#    #+#             */
+/*   Updated: 2026/04/05 16:15:45 by giodos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	exec_env(char **argv, t_env *env)
+{
+	char	*key_plus_path;
+
+	if (argv[1])
+	{
+		ft_printf("env: too many arguments\n");
+		return (1);
+	}
+	while (env)
+	{
+		if (env->value)
+		{
+			key_plus_path = str_join_three(env->key, '=', env->value);
+			if (!key_plus_path)
+				continue;
+			ft_printf("%s\n", key_plus_path);
+			free(key_plus_path);
+		}
+		env = env->next;
+	}
+	return (0);
+}
